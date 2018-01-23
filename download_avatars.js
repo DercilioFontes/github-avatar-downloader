dadf518f49d9b3dffc7b4d28f884bbfa6bbc9669
DercilioFontes <dercilioafontes@gmail.com>
2018-01-23 03:18:42 +0000

    Modified callback function in download_avartas.js to print each avatar_url.

--- download_avatars.js
+++ download_avatars.js
@@ -4,20 +4,31 @@ var secrets = require('./secrets.js');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
+  // Information for request
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'token' + secrets.GITHUB_TOKEN
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
     }
   };
 
+  // resuest passing callback function
   request(options, function(err, res, body) {
-    cb(err, body);
+    var dataContributors = JSON.parse(body);
+    cb(err, dataContributors);
   });
 }
 
+// for loop to print each avarta_url
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log(result);
+  // for(var i = 0; i < result.length; i++) {
+  //   console.log(result[i].avatar_url);
+  // }
+  for (var contributor in result) {
+    console.log("avatar_url:", result[contributor].avatar_url);
+  }
 });
